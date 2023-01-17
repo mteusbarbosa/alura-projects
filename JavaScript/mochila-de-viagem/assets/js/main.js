53552f8cfd518cb9721e139edc1b5ea9c3c1182b
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-17 16:40:21 -0300

    Projeto finalizado

--- JavaScript/mochila-de-viagem/assets/js/main.js
+++ JavaScript/mochila-de-viagem/assets/js/main.js
@@ -18,13 +18,13 @@ form.addEventListener("submit", (event) => {
         "quantidade": quantidade.value
     }
 
-    if(existe){
-      itemAtual.id = existe.id;
-      atualizaElemento(itemAtual);
+    if (existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
 
-      itens[existe.id] = itemAtual;
-    }else{
-        itemAtual.id = itens.length;
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+    } else {
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
         criarElemento(itemAtual);
         itens.push(itemAtual);
     }
@@ -51,10 +51,30 @@ function criarElemento(item) {
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id));
+
     //Adiciona o elemento novoItem dentro da lista 
     lista.appendChild(novoItem);
 }
 
-function atualizaElemento(item){
+function atualizaElemento(item) {
     document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function () {
+        deletaElemento(this.parentNode, id);
+    })
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
