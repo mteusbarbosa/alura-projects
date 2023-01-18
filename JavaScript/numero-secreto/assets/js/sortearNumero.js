dda1bcac8cbc5a9114dcae04773f54fe84dd31b2
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-18 17:30:09 -0300

    Definindo a voz com web speech

--- JavaScript/numero-secreto/assets/js/sortearNumero.js
+++ JavaScript/numero-secreto/assets/js/sortearNumero.js
@@ -6,7 +6,6 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * maiorValor + 1)
 }
 
-console.log('Número secreto:', numeroSecreto);
 const elementoMenorValor = document.getElementById("menor-valor")
 const elementoMaiorValor = document.getElementById("maior-valor")
 
