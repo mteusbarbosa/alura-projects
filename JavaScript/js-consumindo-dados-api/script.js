c1e4efe9feb7f9fac9bb6d4dcfc70056b1691803
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-19 17:14:18 -0300

    Promises

--- JavaScript/js-consumindo-dados-api/script.js
+++ JavaScript/js-consumindo-dados-api/script.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+let consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe')
+        } else
+            console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluído!'));
+
