0f6b5664f94058c4520f515c0a07655abc6775c5
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-19 20:43:34 -0300

    Concerto do erro do catch

--- JavaScript/js-consumindo-dados-api/script.js
+++ JavaScript/js-consumindo-dados-api/script.js
@@ -1,11 +1,18 @@
-let consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe')
-        } else
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
-
+async function buscaEndereco() {
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/01001000/json/`)
+        let consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+buscaEndereco();
+/* Consultar vários CEPs
+let ceps = ['01001000', '59158150'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas)); */
