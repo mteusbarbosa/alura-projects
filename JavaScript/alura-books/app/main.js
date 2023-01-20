99187014fd63009086c376ba3fe50f0c7858a87b
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-20 18:47:08 -0300

    metodo filter

--- JavaScript/alura-books/app/main.js
+++ JavaScript/alura-books/app/main.js
@@ -10,27 +10,3 @@ async function getBuscarLivrosAPI(){
     let livrosComDesconto = aplicarDesconto(livros)
     exibirOsLivrosNaTela(livrosComDesconto)
 }
-
-function exibirOsLivrosNaTela(listaDeLivros){
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-        ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>`
-    });
-}
-
-function aplicarDesconto(livros){
-    const desconto = 0.3;
-    let livrosComDesconto = livros.map(livro => {
-        return {...livro, preco: livro.preco - (livro.preco * desconto)}
-    })
-    return livrosComDesconto;
-}
