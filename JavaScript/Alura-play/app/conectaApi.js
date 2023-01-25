cd784972d141685c6481dac6ebc7fc5fb813e932
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-25 11:07:01 -0300

    Método POST

--- JavaScript/Alura-play/app/conectaApi.js
+++ JavaScript/Alura-play/app/conectaApi.js
@@ -4,6 +4,26 @@ async function listaVideos(){
     return concexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers:{
+            //Define o tipo do arquivo como json
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const concexaoConvertida = await conexao.json();
+    return concexaoConvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
---
+++ JavaScript/Alura-play/app/criarVideo.js
@@ -0,0 +1,21 @@
+import { conectaApi } from "./conectaApi.js";
+const formulario = document.querySelector("[data-formulario]");
+
+async function criarVideo(evento) {
+    //Protege a página de ser recarregada ao clicar no botão de submit
+    evento.preventDefault();
+
+    //Faz o recebimento de todos os dados preenchidos no formulário
+    const imagem = document.querySelector("[data-imagem]").value
+    const url = document.querySelector("[data-url]").value
+    const titulo = document.querySelector("[data-titulo]").value
+    const descricao = Math.floor(Math.random() * 10).toString()
+
+    //Como o cria Video é uma promise, precisa declarar  a funcão como assíncrona e definir o await
+    await conectaApi.criaVideo(titulo, descricao, url, imagem)
+
+    //Quando der certo enviar para a página
+    window.location.href = "../pages/envio-concluido.html"
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento))
