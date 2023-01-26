e1ed7478d0e2deaa8ddac9e5a6845c89088da59b
mteusbarbosa <mateus.bm13@gmail.com>
2023-01-26 17:30:51 -0300

    validação de idade

--- JavaScript/Formulários/monibank-main/js/script.js
+++ JavaScript/Formulários/monibank-main/js/script.js
@@ -1,4 +1,5 @@
 import ehUmCPF from "./valida-CPF.js";
+import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]")
 
@@ -9,6 +10,9 @@ camposDoFormulario.forEach((campo) => {
 function verificaCampo(campo){
     if (campo.name == "cpf" && campo.value.length >=11){
         ehUmCPF(campo)
+    }
+    if (campo.name == "aniversario" && campo.value != ""){
+        ehMaiorDeIdade(campo)
     }
 
 }
