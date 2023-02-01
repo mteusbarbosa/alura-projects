0b950c94d4865266d7f0776b9ddb26fecd38b932
mteusbarbosa <mateus.bm13@gmail.com>
2023-02-01 00:34:53 -0300

    método estático e parâmetro opcional

--- TypeScript/Introdução TypeScript/app/views/view.ts
+++ TypeScript/Introdução TypeScript/app/views/view.ts
@@ -3,13 +3,18 @@ export abstract class View<T> {
   //propriedade
   //modificador protected = só o view tem acesso ao elemento, mas as filhas (herdeiras) podem acessar
   protected elemento: HTMLElement;
+  private escapar = false;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar?: boolean) {
     this.elemento = document.querySelector(seletor);
+    if(escapar) this.escapar = escapar;
   }
 
   public update(model: T): void {
-    const template = this.template(model);
+    let template = this.template(model);
+    if (this.escapar){
+        template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+    }
     this.elemento.innerHTML = template;
   }
 
--- TypeScript/Introdução TypeScript/dist/js/views/view.js
+++ TypeScript/Introdução TypeScript/dist/js/views/view.js
@@ -1,10 +1,16 @@
 //O diamante <T> aceita qualquer tipo que vier
 export class View {
-    constructor(seletor) {
+    constructor(seletor, escapar) {
+        this.escapar = false;
         this.elemento = document.querySelector(seletor);
+        if (escapar)
+            this.escapar = escapar;
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
     }
 }
