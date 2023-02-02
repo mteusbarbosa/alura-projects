7bea866ae3afb6c38b990a470ee917cb1b73689b
mteusbarbosa <mateus.bm13@gmail.com>
2023-02-01 22:12:00 -0300

    decorator domInjector

---
+++ TypeScript/Introdução TypeScript/app/dist/js/decorators/domInject.js
@@ -0,0 +1,2 @@
+export function domInject(target, property, descriptor) {
+}
---
+++ TypeScript/Introdução TypeScript/app/src/decorators/dom-injector.ts
@@ -0,0 +1,13 @@
+export function domInjector(seletor: string) {
+  return function (target: any, propertyKey: string) {
+    const getter = function () {
+      const elemento = document.querySelector(seletor);
+      console.log(
+        `Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`
+      );
+      return elemento;
+    };
+
+    Object.defineProperty(target, propertyKey, { get: getter });
+  };
+}
