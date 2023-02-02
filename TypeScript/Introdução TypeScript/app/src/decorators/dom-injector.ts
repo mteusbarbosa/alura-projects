fa2c03212e27dce70067a24f0871e5b0b99b5b5e
mteusbarbosa <mateus.bm13@gmail.com>
2023-02-01 23:01:52 -0300

    cache decorator

--- TypeScript/Introdução TypeScript/app/src/decorators/dom-injector.ts
+++ TypeScript/Introdução TypeScript/app/src/decorators/dom-injector.ts
@@ -1,10 +1,20 @@
 export function domInjector(seletor: string) {
   return function (target: any, propertyKey: string) {
+    console.log(
+      `Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`
+    );
+    //guarda o elemento HTML
+    let elemento: HTMLElement | null = null;
+
+    //Closure
     const getter = function () {
-      const elemento = document.querySelector(seletor);
-      console.log(
-        `Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`
-      );
+      //se não existe o elemento, elemento recebe o querySelector, mas se existir passa direto
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(seletor);
+        console.log(
+          `Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`
+        );
+      }
       return elemento;
     };
 
